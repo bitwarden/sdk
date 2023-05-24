d4a33fc87b8af935bb50d91aec62766db6d62e99
Colton Hurst <colton@coltonhurst.com>
2023-05-24 18:01:22 -0400

    SM-417: Add project create, update, and delete commands

--- crates/bitwarden/src/sdk/request/projects_request.rs
+++ crates/bitwarden/src/sdk/request/projects_request.rs
@@ -10,7 +10,34 @@ pub struct ProjectGetRequest {
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct ProjectCreateRequest {
+    /// Organization where the project will be created
+    pub organization_id: String,
+
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct ProjectPutRequest {
+    /// ID of the project to modify
+    pub id: String,
+    /// Organization ID of the project to modify
+    pub organization_id: String,
+
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct ProjectsListRequest {
     /// Organization to retrieve all the projects from
     pub organization_id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct ProjectsDeleteRequest {
+    /// IDs of the projects to delete
+    pub ids: Vec<String>,
 }
--- crates/bitwarden/src/sdk/response/projects_response.rs
+++ crates/bitwarden/src/sdk/response/projects_response.rs
@@ -1,4 +1,9 @@
-use bitwarden_api_api::models::{ProjectResponseModel, ProjectResponseModelListResponseModel};
+use bitwarden_api_api::models::{
+    ProjectResponseModel,
+    ProjectResponseModelListResponseModel,
+    BulkDeleteResponseModel,
+    BulkDeleteResponseModelListResponseModel,
+};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -59,6 +64,45 @@ impl ProjectsResponse {
                 .into_iter()
                 .map(|r| ProjectResponse::process_response(r, enc))
                 .collect::<Result<_, _>>()?,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct ProjectsDeleteResponse {
+    pub data: Vec<ProjectDeleteResponse>,
+}
+
+impl ProjectsDeleteResponse {
+    pub(crate) fn process_response(
+        response: BulkDeleteResponseModelListResponseModel,
+    ) -> Result<ProjectsDeleteResponse> {
+        Ok(ProjectsDeleteResponse {
+            data: response
+                .data
+                .unwrap_or_default()
+                .into_iter()
+                .map(ProjectDeleteResponse::process_response)
+                .collect::<Result<_, _>>()?,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct ProjectDeleteResponse {
+    pub id: String,
+    pub error: Option<String>,
+}
+
+impl ProjectDeleteResponse {
+    pub(crate) fn process_response(
+        response: BulkDeleteResponseModel,
+    ) -> Result<ProjectDeleteResponse> {
+        Ok(ProjectDeleteResponse {
+            id: response.id.ok_or(Error::MissingFields)?,
+            error: response.error,
         })
     }
 }

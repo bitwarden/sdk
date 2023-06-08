74eb9dccb6115a86c7f4551b0a6c0677888e8d9c
Hinton <hinton@users.noreply.github.com>
2023-06-08 13:02:31 +0200

    Wip after plaintrip

---
+++ crates/bw/src/main.rs
@@ -0,0 +1,130 @@
+use bitwarden::sdk::{
+    auth::request::PasswordLoginRequest, request::client_settings::ClientSettings,
+};
+use clap::{command, CommandFactory, Parser, Subcommand};
+use color_eyre::eyre::{bail, Result};
+use inquire::Text;
+use render::{Color, Output};
+use std::path::PathBuf;
+
+mod config;
+mod render;
+
+#[derive(Parser, Debug)]
+#[command(name = "Bitwarden Secrets CLI", version, about = "Bitwarden Secrets CLI", long_about = None)]
+struct Cli {
+    // Optional as a workaround for https://github.com/clap-rs/clap/issues/3572
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(short = 'o', long, global = true, value_enum, default_value_t = Output::JSON)]
+    output: Output,
+
+    #[arg(short = 'c', long, global = true, value_enum, default_value_t = Color::Auto)]
+    color: Color,
+
+    #[arg(
+        short = 'f',
+        long,
+        global = true,
+        help = format!("[default: ~/{}/{}] Config file to use", config::DIRECTORY, config::FILENAME)
+    )]
+    config_file: Option<PathBuf>,
+
+    #[arg(short = 'p', long, global = true, env = PROFILE_KEY_VAR_NAME, help="Profile to use from the config file")]
+    profile: Option<String>,
+
+    #[arg(short = 'u', long, global = true, env = SERVER_URL_KEY_VAR_NAME, help="Override the server URL from the config file")]
+    server_url: Option<String>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(long_about = "List items")]
+    Login {},
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    process_commands().await
+}
+
+const PROFILE_KEY_VAR_NAME: &str = "BWS_PROFILE";
+const SERVER_URL_KEY_VAR_NAME: &str = "BWS_SERVER_URL";
+
+async fn process_commands() -> Result<()> {
+    let cli = Cli::parse();
+
+    let color = cli.color.is_enabled();
+    if color {
+        color_eyre::install()?;
+    } else {
+        // Use an empty theme to disable error coloring
+        color_eyre::config::HookBuilder::new()
+            .theme(color_eyre::config::Theme::new())
+            .install()?;
+    }
+
+    let Some(command) = cli.command else {
+        let mut cmd = Cli::command();
+        cmd.print_help()?;
+        return Ok(());
+    };
+
+    let profile = get_config_profile(&cli.server_url, &cli.profile, &cli.config_file)?;
+
+    let settings = profile
+        .map(|p| -> Result<_> {
+            Ok(ClientSettings {
+                identity_url: p.identity_url()?,
+                api_url: p.api_url()?,
+                ..Default::default()
+            })
+        })
+        .transpose()?;
+
+    let mut client = bitwarden::Client::new(settings);
+
+    // And finally we process all the commands which require authentication
+    match command {
+        Commands::Login {} => {
+            let email = Text::new("Email").prompt()?;
+
+            client.password_login(&PasswordLoginRequest { email: email });
+        }
+    }
+
+    Ok(())
+}
+
+fn get_config_profile(
+    server_url: &Option<String>,
+    profile: &Option<String>,
+    config_file: &Option<PathBuf>,
+) -> Result<Option<config::Profile>, color_eyre::Report> {
+    let profile = if let Some(server_url) = server_url {
+        Some(config::Profile::from_url(server_url)?)
+    } else {
+        let profile_defined = profile.is_some();
+
+        let profile_key = if let Some(profile) = profile {
+            profile.to_owned()
+        } else {
+            "".to_string()
+        };
+
+        let config = config::load_config(config_file.as_deref(), config_file.is_some())?;
+        config.select_profile(&profile_key, profile_defined)?
+    };
+    Ok(profile)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}

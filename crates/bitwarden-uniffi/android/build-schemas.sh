8dd726dd2ae481c507c9016642a5ba7402d9f845
Hinton <hinton@users.noreply.github.com>
2023-07-31 20:37:01 +0200

    Tweak path

--- crates/bitwarden-uniffi/android/build-schemas.sh
+++ crates/bitwarden-uniffi/android/build-schemas.sh
@@ -1,6 +1,6 @@
 cargo build --package bitwarden-uniffi --release --all-features
 cargo run -p uniffi-bindgen generate \
-  ../../../target/aarch64-linux-android/release/libbitwarden_uniffi.so \
+  ./sdk/src/main/jniLibs/arm64-v8a/libbitwarden_uniffi.so \
   --library \
   --language kotlin \
   --no-format \

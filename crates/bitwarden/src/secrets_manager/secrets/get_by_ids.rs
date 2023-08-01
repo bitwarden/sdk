49926ecf5854674866b531e662eacbc14d24e2a4
Thomas Avery <tavery@bitwarden.com>
2023-08-01 09:51:29 -0500

    fix cargo check

--- crates/bitwarden/src/secrets_manager/secrets/get_by_ids.rs
+++ crates/bitwarden/src/secrets_manager/secrets/get_by_ids.rs
@@ -3,12 +3,8 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::{
-    client::Client,
-    error::{Error, Result},
-};
-
 use super::SecretsResponse;
+use crate::{client::Client, error::Result};
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
@@ -21,19 +17,16 @@ pub(crate) async fn get_secrets_by_ids(
     client: &mut Client,
     input: SecretsGetRequest,
 ) -> Result<SecretsResponse> {
-    let config = client.get_api_configurations().await;
-
     let request = Some(GetSecretsRequestModel {
         ids: input.ids.clone(),
     });
 
+    let config = client.get_api_configurations().await;
+
     let res =
         bitwarden_api_api::apis::secrets_api::secrets_get_by_ids_post(&config.api, request).await?;
 
-    let enc = client
-        .get_encryption_settings()
-        .as_ref()
-        .ok_or(Error::VaultLocked)?;
+    let enc = client.get_encryption_settings()?;
 
     SecretsResponse::process_response(res, enc)
 }

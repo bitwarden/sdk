6a845bb94371b63ec44217a5dbce6012bbb135d8
Hinton <hinton@users.noreply.github.com>
2023-08-10 19:22:10 +0200

    Remove old files

--- crates/bitwarden-uniffi/android/build-schemas.sh
+++
@@ -1,7 +0,0 @@
-cargo build --package bitwarden-uniffi --release --all-features
-cargo run -p uniffi-bindgen generate \
-  ./sdk/src/main/jniLibs/arm64-v8a/libbitwarden_uniffi.so \
-  --library \
-  --language kotlin \
-  --no-format \
-  --out-dir sdk/src/main/java

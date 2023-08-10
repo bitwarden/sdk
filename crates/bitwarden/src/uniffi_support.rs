3c0a5e5ffd5635c9518bffcc6470a4af332d8664
Hinton <hinton@users.noreply.github.com>
2023-08-10 20:07:24 +0200

    Use uniffi::custom_type

--- crates/bitwarden/src/uniffi_support.rs
+++ crates/bitwarden/src/uniffi_support.rs
@@ -1,71 +1,62 @@
 use std::{num::NonZeroU32, str::FromStr};
 
-use uniffi::{FfiConverter, MetadataBuffer, RustBuffer};
 use uuid::Uuid;
 
-use crate::{crypto::CipherString, error::Error, UniFfiTag};
+use crate::{crypto::CipherString, error::Error, UniffiCustomTypeConverter};
 
-// Could probably be replaced with https://github.com/mozilla/uniffi-rs/pull/1661
-macro_rules! ffi_converter_impl {
-    ($type:ty, $intermediate_rust:ty, $intermediate_ffi:ty, $to_intermediate:expr, $from_intermediate:expr) => {
-        unsafe impl FfiConverter<UniFfiTag> for $type {
-            uniffi::ffi_converter_default_return!(crate::UniFfiTag);
+uniffi::custom_type!(NonZeroU32, u32);
 
-            type FfiType = $intermediate_ffi;
+impl UniffiCustomTypeConverter for NonZeroU32 {
+    type Builtin = u32;
 
-            fn lower(obj: Self) -> Self::FfiType {
-                <$intermediate_rust as FfiConverter<UniFfiTag>>::lower(($to_intermediate)(obj))
-            }
-            fn try_lift(v: Self::FfiType) -> uniffi::Result<Self> {
-                let s = <$intermediate_rust as FfiConverter<UniFfiTag>>::try_lift(v)?;
-                Ok(($from_intermediate)(s)?)
-            }
-            fn write(obj: Self, buf: &mut Vec<u8>) {
-                <$intermediate_rust as FfiConverter<UniFfiTag>>::write(
-                    ($to_intermediate)(obj),
-                    buf,
-                );
-            }
-            fn try_read(buf: &mut &[u8]) -> uniffi::Result<Self> {
-                let s = <$intermediate_rust as FfiConverter<UniFfiTag>>::try_read(buf)?;
-                Ok(($from_intermediate)(s)?)
-            }
-            const TYPE_ID_META: MetadataBuffer =
-                MetadataBuffer::from_code(uniffi::metadata::codes::TYPE_CUSTOM)
-                    .concat_str("bitwarden")
-                    .concat_str(stringify!($type))
-                    .concat(<$intermediate_rust as FfiConverter<UniFfiTag>>::TYPE_ID_META);
-        }
-    };
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Self::new(val).ok_or(Error::Internal("Number is zero").into())
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.get()
+    }
 }
 
-ffi_converter_impl!(
-    Uuid,
-    String,
-    RustBuffer,
-    |u: Self| u.to_string(),
-    |s: String| Self::parse_str(&s)
-);
+uniffi::custom_type!(CipherString, String);
 
-ffi_converter_impl!(
-    CipherString,
-    String,
-    RustBuffer,
-    |u: Self| u.to_string(),
-    |s: String| { Self::from_str(&s) }
-);
+impl UniffiCustomTypeConverter for CipherString {
+    type Builtin = String;
 
-// Can't have a generic in the type
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Self::from_str(&val).map_err(|e| e.into())
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.to_string()
+    }
+}
+
 type DateTime = chrono::DateTime<chrono::Utc>;
-// TODO: Convert to systemtime
-ffi_converter_impl!(
-    DateTime,
-    std::time::SystemTime,
-    RustBuffer,
-    |u: Self| u.into(),
-    |s: std::time::SystemTime| { Ok::<_, Error>(Self::from(s)) }
-);
+uniffi::custom_type!(DateTime, std::time::SystemTime);
 
-ffi_converter_impl!(NonZeroU32, u32, u32, |u: Self| u.get(), |s: u32| {
-    Self::new(s).ok_or(Error::Internal("Number is zero"))
-});
+impl UniffiCustomTypeConverter for chrono::DateTime<chrono::Utc> {
+    type Builtin = std::time::SystemTime;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Ok(Self::from(val))
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.into()
+    }
+}
+
+uniffi::custom_type!(Uuid, String);
+
+impl UniffiCustomTypeConverter for Uuid {
+    type Builtin = String;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Uuid::parse_str(val.as_str()).map_err(|e| e.into())
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.to_string()
+    }
+}

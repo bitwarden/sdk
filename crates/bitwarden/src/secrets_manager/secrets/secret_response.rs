76865d9b6857070d7e66f08755c9b02180fe8756
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2023-08-28 10:38:11 -0500

    [SM-865] Part 2 Use new get secrets by IDs endpoint (#150)
    * Add auto generated Api bindings
    
    * Expose new endpoint methods in SDK
    
    * Use in bws CLI
    
    * Expose new command in JSON schemas
    
    * restructure and rename response
    
    * Expose getByIds in napi
    
    * fix cargo check
    
    * Add openapi auto generation
    
    * Regenerated open api doc
    
    * revert back to original binding.js
    
    * remove .clone()
    
    * Add to changelog
    
    * run cargo fmt

--- crates/bitwarden/src/secrets_manager/secrets/secret_response.rs
+++ crates/bitwarden/src/secrets_manager/secrets/secret_response.rs
@@ -1,4 +1,6 @@
-use bitwarden_api_api::models::SecretResponseModel;
+use bitwarden_api_api::models::{
+    BaseSecretResponseModel, BaseSecretResponseModelListResponseModel, SecretResponseModel,
+};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -28,6 +30,23 @@ pub struct SecretResponse {
 impl SecretResponse {
     pub(crate) fn process_response(
         response: SecretResponseModel,
+        enc: &EncryptionSettings,
+    ) -> Result<SecretResponse> {
+        let base = BaseSecretResponseModel {
+            object: response.object,
+            id: response.id,
+            organization_id: response.organization_id,
+            key: response.key,
+            value: response.value,
+            note: response.note,
+            creation_date: response.creation_date,
+            revision_date: response.revision_date,
+            projects: response.projects,
+        };
+        Self::process_base_response(base, enc)
+    }
+    pub(crate) fn process_base_response(
+        response: BaseSecretResponseModel,
         enc: &EncryptionSettings,
     ) -> Result<SecretResponse> {
         let org_id = response.organization_id;
@@ -64,6 +83,28 @@ impl SecretResponse {
 
             creation_date: response.creation_date.ok_or(Error::MissingFields)?,
             revision_date: response.revision_date.ok_or(Error::MissingFields)?,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct SecretsResponse {
+    pub data: Vec<SecretResponse>,
+}
+
+impl SecretsResponse {
+    pub(crate) fn process_response(
+        response: BaseSecretResponseModelListResponseModel,
+        enc: &EncryptionSettings,
+    ) -> Result<SecretsResponse> {
+        Ok(SecretsResponse {
+            data: response
+                .data
+                .unwrap_or_default()
+                .into_iter()
+                .map(|r| SecretResponse::process_base_response(r, enc))
+                .collect::<Result<_, _>>()?,
         })
     }
 }

5ee2df84fae40dd5c5a789a77b97d4600190cfe0
Hinton <hinton@users.noreply.github.com>
2023-08-28 11:15:33 +0200

    Build a universal binary since it's required

--- languages/swift/build.sh
+++ languages/swift/build.sh
@@ -5,11 +5,17 @@ rm -r BitwardenFFI.xcframework
 rm -r tmp
 
 mkdir tmp
+mkdir -p tmp/target/universal-ios-sim/release
 
 # Build native library
 cargo build --package bitwarden-uniffi --target aarch64-apple-ios-sim --release
 cargo build --package bitwarden-uniffi --target aarch64-apple-ios --release
 cargo build --package bitwarden-uniffi --target x86_64-apple-ios --release
+
+# Create universal libraries
+lipo -create ../../target/aarch64-apple-ios-sim/release/libbitwarden_uniffi.dylib \
+  ../../target/x86_64-apple-ios/release/libbitwarden_uniffi.dylib  \
+  -output ./tmp/target/universal-ios-sim/release/libbitwarden_uniffi.dylib
 
 # Generate swift bindings
 cargo run -p uniffi-bindgen generate \
@@ -31,11 +37,9 @@ cat ./tmp/bindings/BitwardenFFI.modulemap ./tmp/bindings/BitwardenCoreFFI.module
 
 # Build xcframework
 xcodebuild -create-xcframework \
-  -library ../../target/aarch64-apple-ios-sim/release/libbitwarden_uniffi.dylib \
-  -headers ./tmp/Headers \
   -library ../../target/aarch64-apple-ios/release/libbitwarden_uniffi.dylib \
   -headers ./tmp/Headers \
-  -library ../../target/x86_64-apple-ios/release/libbitwarden_uniffi.dylib \
+  -library ./tmp/target/universal-ios-sim/release/libbitwarden_uniffi.dylib \
   -headers ./tmp/Headers \
   -output ./BitwardenFFI.xcframework
 

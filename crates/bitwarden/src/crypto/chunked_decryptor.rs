ad1841780aac63edb8320b87f9db8a047348d87d
Daniel García <dani-garcia@users.noreply.github.com>
2023-08-29 13:32:32 +0200

    Improve partial chunk processing

--- crates/bitwarden/src/crypto/chunked_decryptor.rs
+++ crates/bitwarden/src/crypto/chunked_decryptor.rs
@@ -52,11 +52,11 @@ const MAX_BLOCK_SIZE: usize = 16;
 pub struct ChunkedDecryptor {
     enc_type: EncryptionType,
 
-    // Block size of the cipher used, the data passed to the decryptor must this size
+    // Block size of the cipher used, the data passed to the decryptor must be exactly this size
     block_size: usize,
 
-    // Buffer for storing the last full block and the last partial block (if any) from the previous chunk
-    buf: [u8; 2 * MAX_BLOCK_SIZE],
+    // Buffer for storing the last block from the previous chunk, either partially or in full
+    buf: [u8; MAX_BLOCK_SIZE],
     buf_len: usize,
 }
 
@@ -70,7 +70,7 @@ impl ChunkedDecryptor {
         let block_size;
 
         // The first byte of the message indicates the encryption type
-        let Some(enc_type_num) = initial_chunk.first() else {
+        let Some(&enc_type_num) = initial_chunk.first() else {
             return Err(EncStringParseError::InvalidType {
                 enc_type: "Missing".to_string(),
                 parts: 1,
@@ -131,7 +131,7 @@ impl ChunkedDecryptor {
         let mut decryptor = Self {
             enc_type,
             block_size,
-            buf: [0u8; 2 * MAX_BLOCK_SIZE],
+            buf: [0u8; MAX_BLOCK_SIZE],
             buf_len: 0,
         };
         // Process the rest of the initial chunk
@@ -159,40 +159,37 @@ impl ChunkedDecryptor {
                 let mut result = Vec::with_capacity(chunk.len() + self.block_size);
 
                 let mut process_block = |block: &[u8]| {
-                    let block: [u8; 16] = block.try_into().unwrap();
-                    let mut block = block.into();
+                    debug_assert_eq!(block.len(), self.block_size);
+
+                    let mut block = GenericArray::clone_from_slice(block);
                     decryptor.decrypt_block_mut(&mut block);
                     result.extend_from_slice(&block);
                 };
 
                 let skip_initial_bytes = if self.buf_len > 0 {
-                    // Process previous full block
-                    let previous_full_block = &self.buf[..self.block_size];
-                    process_block(previous_full_block);
+                    // Process partial block if there is one. This will also process a full block if  buf_len == block_size
+                    let bytes_to_complete_partial = self.block_size - self.buf_len;
 
-                    // Process partial block if there is one
-                    if self.buf_len > self.block_size {
-                        let bytes_to_complete_partial = self.buf.len() - self.buf_len;
+                    // Fill up the partial block with the first bytes of the chunk
+                    self.buf[self.buf_len..self.block_size]
+                        .copy_from_slice(&chunk[0..bytes_to_complete_partial]);
 
-                        // Fill up the partial block with the first bytes of the chunk
-                        self.buf[self.buf_len..]
-                            .copy_from_slice(&chunk[0..bytes_to_complete_partial]);
+                    // Process the now filled partial block
+                    process_block(&self.buf[..self.block_size]);
 
-                        // Process the now filled partial block
-                        let previous_partial_block = &self.buf[self.block_size..];
-                        process_block(previous_partial_block);
-
-                        bytes_to_complete_partial
-                    } else {
-                        0
-                    }
+                    bytes_to_complete_partial
                 } else {
                     0
                 };
 
                 // Check how many bytes we need to process the previous partial data and the current chunk
                 let full_chunk_size = chunk.len() - skip_initial_bytes;
-                let remainder_bytes = (full_chunk_size % self.block_size) + self.block_size;
+                let mut remainder_bytes = full_chunk_size % self.block_size;
+
+                // Make sure we leave at least one block unprocessed, to remove the padding later
+                if remainder_bytes == 0 {
+                    remainder_bytes = self.block_size;
+                }
 
                 let chunk_to_process = &chunk[skip_initial_bytes..(chunk.len() - remainder_bytes)];
 
@@ -215,17 +212,15 @@ impl ChunkedDecryptor {
             EncryptionType::AesCbc256_B64 { .. } => unimplemented!(),
             EncryptionType::AesCbc128_HmacSha256_B64 { .. } => unimplemented!(),
             EncryptionType::AesCbc256_HmacSha256_B64 { decryptor, .. } => {
-                #[allow(clippy::comparison_chain)]
-                if self.buf_len > self.block_size {
-                    return Err(Error::Internal("Partial block at the end of the data"));
-                } else if self.buf_len == self.block_size {
-                    let block: [u8; 16] = self.buf[..self.block_size].try_into().unwrap();
-                    let mut block = block.into();
+                if self.buf_len == self.block_size {
+                    let mut block = GenericArray::clone_from_slice(&self.buf[..self.block_size]);
                     decryptor.decrypt_block_mut(&mut block);
 
                     Pkcs7::unpad(&block).unwrap().to_vec()
+                } else if self.buf_len == 0 {
+                    return Err(Error::Internal("Missing block at the end of the data"));
                 } else {
-                    Vec::new()
+                    return Err(Error::Internal("Partial block at the end of the data"));
                 }
             }
         };
@@ -279,6 +274,8 @@ pub fn decrypt_file(
 
     let chunk = decryptor.finalize()?;
     decrypted_file.write_all(&chunk)?;
+
+    decrypted_file.flush()?;
 
     Ok(())
 }

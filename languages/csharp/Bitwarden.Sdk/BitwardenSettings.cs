436cc7efa477fdade473059a569ea4deee3a73f4
Hinton <hinton@users.noreply.github.com>
2023-09-21 20:52:35 +0200

    Undo tabs

--- languages/csharp/Bitwarden.Sdk/BitwardenSettings.cs
+++ languages/csharp/Bitwarden.Sdk/BitwardenSettings.cs
@@ -3,13 +3,12 @@
 public class BitwardenSettings
 {
     /// <summary>
-    ///     The api url of the targeted Bitwarden instance. Defaults to `https://api.bitwarden.com`
+    /// The api url of the targeted Bitwarden instance. Defaults to `https://api.bitwarden.com`
     /// </summary>
     public string? ApiUrl { get; set; }
 
     /// <summary>
-    ///     The identity url of the targeted Bitwarden instance. Defaults to
-    ///     `https://identity.bitwarden.com`
+    /// The identity url of the targeted Bitwarden instance. Defaults to `https://identity.bitwarden.com`
     /// </summary>
     public string? IdentityUrl { get; set; }
 }

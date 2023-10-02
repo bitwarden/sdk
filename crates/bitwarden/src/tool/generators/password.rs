f02164710fd4108db245b62a074b626ec204920f
Daniel García <dani-garcia@users.noreply.github.com>
2023-10-02 13:53:59 +0200

    Extract charset logic to a separate struct and add tests

--- crates/bitwarden/src/tool/generators/password.rs
+++ crates/bitwarden/src/tool/generators/password.rs
@@ -1,4 +1,4 @@
-use crate::error::Result;
+use crate::error::{Error, Result};
 use rand::seq::SliceRandom;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -43,85 +43,182 @@ pub struct PassphraseGeneratorRequest {
 
 const DEFAULT_PASSWORD_LENGTH: u8 = 16;
 
-const UPPER_CHARS_AMBIGUOUS: [char; 2] = ['I', 'O'];
-const UPPER_CHARS: [char; 24] = [
+const UPPER_CHARS_AMBIGUOUS: &[char] = &['I', 'O'];
+const UPPER_CHARS: &[char] = &[
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U',
     'V', 'W', 'X', 'Y', 'Z',
 ];
-const LOWER_CHARS_AMBIGUOUS: [char; 1] = ['l'];
-const LOWER_CHARS: [char; 25] = [
+const LOWER_CHARS_AMBIGUOUS: &[char] = &['l'];
+const LOWER_CHARS: &[char] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z',
 ];
-const NUMBER_CHARS_AMBIGUOUS: [char; 2] = ['0', '1'];
-const NUMBER_CHARS: [char; 8] = ['2', '3', '4', '5', '6', '7', '8', '9'];
-const SPECIAL_CHARS: [char; 8] = ['!', '@', '#', '$', '%', '^', '&', '*'];
+const NUMBER_CHARS_AMBIGUOUS: &[char] = &['0', '1'];
+const NUMBER_CHARS: &[char] = &['2', '3', '4', '5', '6', '7', '8', '9'];
+const SPECIAL_CHARS: &[char] = &['!', '@', '#', '$', '%', '^', '&', '*'];
+
+struct PasswordGeneratorCharSet {
+    lower: Vec<char>,
+    upper: Vec<char>,
+    number: Vec<char>,
+    special: Vec<char>,
+    all: Vec<char>,
+}
+
+impl PasswordGeneratorCharSet {
+    fn new(lower: bool, upper: bool, number: bool, special: bool, avoid_ambiguous: bool) -> Self {
+        fn chars(
+            enabled: bool,
+            chars: &[char],
+            ambiguous: &[char],
+            avoid_ambiguous: bool,
+        ) -> Vec<char> {
+            if !enabled {
+                return Vec::new();
+            }
+            let mut chars = chars.to_vec();
+            if !avoid_ambiguous {
+                chars.extend_from_slice(ambiguous);
+            }
+            chars
+        }
+        let lower = chars(lower, LOWER_CHARS, LOWER_CHARS_AMBIGUOUS, avoid_ambiguous);
+        let upper = chars(upper, UPPER_CHARS, UPPER_CHARS_AMBIGUOUS, avoid_ambiguous);
+        let number = chars(
+            number,
+            NUMBER_CHARS,
+            NUMBER_CHARS_AMBIGUOUS,
+            avoid_ambiguous,
+        );
+        let special = chars(special, SPECIAL_CHARS, &[], avoid_ambiguous);
+        let all = lower
+            .iter()
+            .chain(&upper)
+            .chain(&number)
+            .chain(&special)
+            .copied()
+            .collect();
+
+        Self {
+            lower,
+            upper,
+            number,
+            special,
+            all,
+        }
+    }
+}
 
 pub(super) fn password(input: PasswordGeneratorRequest) -> Result<String> {
-    // Generate all character dictionaries
-    fn gen_chars(chars: &[char], ambiguous: &[char], avoid_ambiguous: bool) -> Vec<char> {
-        let mut chars = chars.to_vec();
-        if !avoid_ambiguous {
-            chars.extend_from_slice(ambiguous);
-        }
-        chars
+    // We always have to have at least one character set enabled
+    if !input.lowercase || !input.uppercase && !input.numbers && !input.special {
+        return Err(Error::Internal(
+            "At least one character set must be enabled",
+        ));
     }
-    let avoid_ambiguous = input.avoid_ambiguous.unwrap_or(false);
-    let lower_chars = gen_chars(&LOWER_CHARS, &LOWER_CHARS_AMBIGUOUS, avoid_ambiguous);
-    let upper_chars = gen_chars(&UPPER_CHARS, &UPPER_CHARS_AMBIGUOUS, avoid_ambiguous);
-    let number_chars = gen_chars(&NUMBER_CHARS, &NUMBER_CHARS_AMBIGUOUS, avoid_ambiguous);
-    let all_chars = lower_chars
-        .iter()
-        .chain(&upper_chars)
-        .chain(&number_chars)
-        .chain(&SPECIAL_CHARS)
-        .collect::<Vec<_>>();
 
-    // We always have to have at least one character type
-    let lowercase = input.lowercase || (!input.uppercase && !input.numbers && !input.special);
+    // Generate all character dictionaries
+    let chars = PasswordGeneratorCharSet::new(
+        input.lowercase,
+        input.uppercase,
+        input.numbers,
+        input.special,
+        input.avoid_ambiguous.unwrap_or(false),
+    );
 
-    // Sanitize the minimum values
+    // Make sure the minimum values are zero when the character
+    // set is disabled, and at least one when it's enabled
     fn get_minimum(min: Option<u8>, enabled: bool) -> u8 {
         if enabled {
-            // Make sure there's at least one
             u8::max(min.unwrap_or(1), 1)
         } else {
             0
         }
     }
-    let min_lowercase = get_minimum(input.min_lowercase, lowercase);
+    let min_lowercase = get_minimum(input.min_lowercase, input.lowercase);
     let min_uppercase = get_minimum(input.min_uppercase, input.uppercase);
     let min_number = get_minimum(input.min_number, input.numbers);
     let min_special = get_minimum(input.min_special, input.special);
 
-    // Sanitize the length value
+    // Check that the minimum lengths aren't larger than the password length
     let min_length = min_lowercase + min_uppercase + min_number + min_special;
-    let length = u8::max(input.length.unwrap_or(DEFAULT_PASSWORD_LENGTH), min_length);
+    let length = input.length.unwrap_or(DEFAULT_PASSWORD_LENGTH);
+    if min_length > length {
+        return Err(Error::Internal(
+            "Password length can't be less than the sum of the minimums",
+        ));
+    }
 
     // Generate the minimum chars of each type, then generate the rest to fill the expected length
-    let mut chars = Vec::with_capacity(length as usize);
+    let mut buf = Vec::with_capacity(length as usize);
     let mut rand = rand::thread_rng();
 
     for _ in 0..min_lowercase {
-        chars.push(*lower_chars.choose(&mut rand).expect("slice is not empty"));
+        buf.push(*chars.lower.choose(&mut rand).expect("slice is not empty"));
     }
     for _ in 0..min_uppercase {
-        chars.push(*upper_chars.choose(&mut rand).expect("slice is not empty"));
+        buf.push(*chars.upper.choose(&mut rand).expect("slice is not empty"));
     }
     for _ in 0..min_number {
-        chars.push(*number_chars.choose(&mut rand).expect("slice is not empty"));
+        buf.push(*chars.number.choose(&mut rand).expect("slice is not empty"));
     }
     for _ in 0..min_special {
-        chars.push(*SPECIAL_CHARS.choose(&mut rand).expect("slice is not empty"));
+        buf.push(*chars.special.choose(&mut rand).expect("slice is not empty"));
     }
     for _ in min_length..length {
-        chars.push(**all_chars.choose(&mut rand).expect("slice is not empty"));
+        buf.push(*chars.all.choose(&mut rand).expect("slice is not empty"));
     }
 
-    chars.shuffle(&mut rand);
-    Ok(chars.iter().collect())
+    buf.shuffle(&mut rand);
+    Ok(buf.iter().collect())
 }
 
 pub(super) fn passphrase(_input: PassphraseGeneratorRequest) -> Result<String> {
     Ok("correct-horse-battery-staple".to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // We convert the slices to Strings to be able to use `contains`
+    // This wouldn't work if the character sets were ordered differently, but that's not the case for us
+    fn to_string(chars: &[char]) -> String {
+        chars.iter().collect()
+    }
+
+    #[test]
+    fn test_password_characters() {
+        // All characters excluding ambiguous
+        let set = PasswordGeneratorCharSet::new(true, true, true, true, true);
+        assert_eq!(set.lower, LOWER_CHARS);
+        assert_eq!(set.upper, UPPER_CHARS);
+        assert_eq!(set.number, NUMBER_CHARS);
+        assert_eq!(set.special, SPECIAL_CHARS);
+
+        // All characters including ambiguous
+        let set = PasswordGeneratorCharSet::new(true, true, true, true, false);
+        assert!(to_string(&set.lower).contains(&to_string(LOWER_CHARS)));
+        assert!(to_string(&set.lower).contains(&to_string(LOWER_CHARS_AMBIGUOUS)));
+        assert!(to_string(&set.upper).contains(&to_string(UPPER_CHARS)));
+        assert!(to_string(&set.upper).contains(&to_string(UPPER_CHARS_AMBIGUOUS)));
+        assert!(to_string(&set.number).contains(&to_string(NUMBER_CHARS)));
+        assert!(to_string(&set.number).contains(&to_string(NUMBER_CHARS_AMBIGUOUS)));
+        assert_eq!(set.special, SPECIAL_CHARS);
+
+        // Only lowercase
+        let set = PasswordGeneratorCharSet::new(true, false, false, false, true);
+        assert_eq!(set.lower, LOWER_CHARS);
+        assert_eq!(set.upper, Vec::new());
+        assert_eq!(set.number, Vec::new());
+        assert_eq!(set.special, Vec::new());
+
+        // Only uppercase including ambiguous
+        let set = PasswordGeneratorCharSet::new(false, true, false, false, false);
+        assert_eq!(set.lower, Vec::new());
+        assert!(to_string(&set.upper).contains(&to_string(UPPER_CHARS)));
+        assert!(to_string(&set.upper).contains(&to_string(UPPER_CHARS_AMBIGUOUS)));
+        assert_eq!(set.number, Vec::new());
+        assert_eq!(set.special, Vec::new());
+    }
 }

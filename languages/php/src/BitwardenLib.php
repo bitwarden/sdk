651a2d3fc9804ee26e7b187e09a49ba36a78d6b7
Miloš Trifunović <84377717+milost77@users.noreply.github.com>
2023-10-31 22:41:21 +0100

    adding workflow (#1)
    * adding workflow

--- languages/php/src/BitwardenLib.php
+++ languages/php/src/BitwardenLib.php
@@ -5,6 +5,7 @@ namespace Bitwarden\Sdk;
 use Bitwarden\Sdk\Schemas\ClientSettings;
 use Bitwarden\Sdk\Schemas\Command;
 use FFI;
+use Swaggest\JsonDiff\Exception;
 use Swaggest\JsonSchema\JsonSchema;
 
 
@@ -13,13 +14,44 @@ class BitwardenLib
     public FFI $ffi;
     public FFI\CData $handle;
 
+    /**
+     * @throws \Exception
+     */
     public function __construct()
     {
+        $lib_file = null;
+
+        if (PHP_OS === 'WINNT') {
+            $lib_file = '/lib/windows-x64/bitwarden_c.dll';
+            if (file_exists($lib_file) == false) {
+                $lib_file = __DIR__.'../../../../target/debug/bitwarden_c.dll';
+            }
+        } elseif (PHP_OS === 'Linux') {
+            $lib_file = '/lib/ubuntu-x64/libbitwarden_c.so';
+            if (file_exists($lib_file) == false) {
+                $lib_file = __DIR__.'../../../../target/debug/libbitwarden_c.so';
+            }
+        } elseif (PHP_OS === 'Darwin') {
+            $architecture = trim(exec('uname -m'));
+            if ($architecture === 'x86_64' || $architecture === 'amd64') {
+                $lib_file = __DIR__.'/lib/macos-x64/libbitwarden_c.dylib';
+            } elseif ($architecture === 'arm64') {
+                $lib_file = __DIR__.'/lib/macos-arm64/libbitwarden_c.dylib';
+            }
+            if (file_exists($lib_file) == false) {
+                $lib_file = __DIR__.'../../../../target/debug/libbitwarden_c.dylib';
+            }
+        }
+
+        if ($lib_file == null || is_file($lib_file) == false) {
+            throw new \Exception("Lib file not found");
+        }
+
         $this->ffi = FFI::cdef('
             void* init(const char* param);
             char* run_command(void* c_str_ptr, void* client_ptr);
             void free_mem(void* client_ptr);',
-            __DIR__ . '/libbitwarden_c.dylib'
+            $lib_file
         );
     }
 

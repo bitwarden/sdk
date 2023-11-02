f664548d8f921c52b1d52208ae202010f0f39d57
Hinton <hinton@users.noreply.github.com>
2023-11-02 15:30:26 +0100

    Extract JWT to it's own file

--- crates/bitwarden/src/auth/login/access_token.rs
+++ crates/bitwarden/src/auth/login/access_token.rs
@@ -6,11 +6,12 @@ use crate::{
     auth::{
         api::{request::AccessTokenRequest, response::IdentityTokenResponse},
         login::{response::two_factor::TwoFactorProviders, PasswordLoginResponse},
+        JWTToken,
     },
     client::{AccessToken, LoginMethod, ServiceAccountLoginMethod},
     crypto::{EncString, KeyDecryptable, SymmetricCryptoKey},
     error::{Error, Result},
-    util::{decode_token, BASE64_ENGINE},
+    util::BASE64_ENGINE,
     Client,
 };
 
@@ -44,7 +45,7 @@ pub(crate) async fn access_token_login(
 
         let encryption_key = SymmetricCryptoKey::try_from(encryption_key.as_slice())?;
 
-        let access_token_obj = decode_token(&r.access_token)?;
+        let access_token_obj: JWTToken = r.access_token.parse()?;
 
         // This should always be Some() when logging in with an access token
         let organization_id = access_token_obj

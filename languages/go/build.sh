b7ef4b8e72b386e8212b24512a6122d80401cb5e
Boris Bujak <boris.bujak@carvana.com>
2023-11-14 12:49:30 +0100

    remove build.sh and custom lib

--- languages/go/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "$BITWARDEN_LIB_PATH" ]; then
-    sed "s/{{.LibPath}}/$BITWARDEN_LIB_PATH/g" internal/cinterface/bitwarden_library.go > internal/cinterface/bitwarden_library.go
-    go build -tags custom
-else
-    go build
-fi

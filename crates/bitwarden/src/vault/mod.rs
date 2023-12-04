48aeb58eb63050c658259bfd188dd5fae9da9488
Oscar Hinton <Hinton@users.noreply.github.com>
2023-12-04 18:01:57 +0100

    Initial TOTP implementation (#392)

--- crates/bitwarden/src/vault/mod.rs
+++ crates/bitwarden/src/vault/mod.rs
@@ -10,4 +10,5 @@ pub use collection::{Collection, CollectionView};
 pub use folder::{Folder, FolderView};
 pub use password_history::{PasswordHistory, PasswordHistoryView};
 pub use send::{Send, SendListView, SendView};
-pub use totp::{generate_totp, TotpResponse};
+pub(crate) use totp::generate_totp;
+pub use totp::TotpResponse;

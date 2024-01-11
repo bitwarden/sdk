73a0856c3f62b46851d65ea6898f307da96d9b55
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-01-11 04:16:21 -0800

    add `state_file_path` param to `access_token_login`

--- languages/python/bitwarden_sdk/bitwarden_client.py
+++ languages/python/bitwarden_sdk/bitwarden_client.py
@@ -12,9 +12,10 @@ class BitwardenClient:
             settings_json = json.dumps(settings.to_dict())
             self.inner = bitwarden_py.BitwardenClient(settings_json)
 
-    def access_token_login(self, access_token: str):
+    def access_token_login(self, access_token: str,
+                           state_file_path: str = None) -> ResponseForAccessTokenLoginResponse:
         self._run_command(
-            Command(access_token_login=AccessTokenLoginRequest(access_token))
+            Command(access_token_login=AccessTokenLoginRequest(access_token, state_file_path))
         )
 
     def secrets(self):

dde7039bb4bb7faa468507d79823017a7cd83404
Hinton <hinton@users.noreply.github.com>
2024-01-22 14:06:19 +0100

    Use to_vec

--- crates/bitwarden-crypto/src/keys/device_key.rs
+++ crates/bitwarden-crypto/src/keys/device_key.rs
@@ -32,7 +32,7 @@ impl DeviceKey {
         let device_private_key = AsymmetricCryptoKey::generate(&mut rng);
 
         // Encrypt both the key and mac_key of the user key
-        let data = [user_key.key, user_key.mac_key.unwrap_or_default()].concat();
+        let data = user_key.to_vec();
 
         let protected_user_key =
             AsymmetricEncString::encrypt_rsa2048_oaep_sha1(&data, &device_private_key)?;

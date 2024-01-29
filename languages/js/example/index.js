d6c6c9726d8ed16e9f721f4b4c2a1a2617bca61b
Daniel García <dani-garcia@users.noreply.github.com>
2024-01-29 11:11:24 +0100

    Prettier formatting

--- languages/js/example/index.js
+++ languages/js/example/index.js
@@ -1,31 +1,32 @@
-
-const { BitwardenClient: BitwardenClientWasm, LogLevel } = require('@bitwarden/sdk-wasm');
-const sdk = require('@bitwarden/sdk-client');
-
+const { BitwardenClient: BitwardenClientWasm, LogLevel } = require("@bitwarden/sdk-wasm");
+const sdk = require("@bitwarden/sdk-client");
 
 async function main() {
-    const settings = {
-        apiUrl: process.env.API_URL,
-        identityUrl: process.env.IDENTITY_URL
-    };
+  const settings = {
+    apiUrl: process.env.API_URL,
+    identityUrl: process.env.IDENTITY_URL,
+  };
 
-    const client = new sdk.BitwardenClient(new BitwardenClientWasm(JSON.stringify(settings), LogLevel.Debug));
+  const client = new sdk.BitwardenClient(
+    new BitwardenClientWasm(JSON.stringify(settings), LogLevel.Debug),
+  );
 
-    const organization_id = process.env.ORGANIZATION_ID;
-    await client.accessTokenLogin(process.env.ACCESS_TOKEN);
+  const organization_id = process.env.ORGANIZATION_ID;
+  await client.accessTokenLogin(process.env.ACCESS_TOKEN);
 
+  const project = await client.projects().create("test", organization_id);
+  const projects = await client.projects().list(organization_id);
+  console.log(projects.data);
 
-    const project = await client.projects().create('test', organization_id);
-    const projects = await client.projects().list(organization_id);
-    console.log(projects.data);
+  const secret = await client
+    .secrets()
+    .create("test-secret", "My secret!", "This is my secret", [project.id], organization_id);
+  const secrets = await client.secrets().list(organization_id);
+  console.log(secrets.data);
 
-    const secret = await client.secrets().create('test-secret', "My secret!", "This is my secret", [project.id], organization_id);
-    const secrets = await client.secrets().list(organization_id);
-    console.log(secrets.data);
+  console.log(project, secret);
 
-    console.log(project, secret);
-
-   await client.projects().delete([project.id]);
-   await client.secrets().delete([secret.id]);
+  await client.projects().delete([project.id]);
+  await client.secrets().delete([secret.id]);
 }
 main();

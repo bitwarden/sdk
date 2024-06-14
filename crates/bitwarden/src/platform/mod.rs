943f4163044b4868a0966a92f0a802b116e7afee
Hinton <hinton@users.noreply.github.com>
2024-06-14 10:31:54 +0200

    Move fido behind uniffi flag

--- crates/bitwarden/src/platform/mod.rs
+++ crates/bitwarden/src/platform/mod.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "uniffi")]
 pub mod client_fido;
 pub mod client_platform;
 mod generate_fingerprint;
@@ -9,6 +10,7 @@ pub(crate) use get_user_api_key::get_user_api_key;
 pub use get_user_api_key::UserApiKeyResponse;
 pub use secret_verification_request::SecretVerificationRequest;
 
+#[cfg(feature = "uniffi")]
 pub mod fido2 {
     pub use bitwarden_fido::*;
 }

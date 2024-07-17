ca3e43dff776fe838d919c3cdcade359e64669dd
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-17 14:05:33 +0200

    chore: clean up in cipher_data

--- crates/bitwarden-vault/src/cipher_data/mod.rs
+++ crates/bitwarden-vault/src/cipher_data/mod.rs
@@ -1,20 +1,5 @@
-use bitwarden_core::VaultLocked;
-use bitwarden_crypto::CryptoError;
-use thiserror::Error;
-
-// pub mod domain;
 mod v1;
 mod v2;
 mod version_agnostic;
 
 pub use version_agnostic::VersionedCipherData;
-
-// #[derive(Debug, Error)]
-// pub enum MigrationError {
-//     #[error(transparent)]
-//     VaultLocked(#[from] VaultLocked),
-// }
-
-pub trait Migrator<From, To> {
-    async fn migrate_from(&self, from: From) -> Result<To, CryptoError>;
-}

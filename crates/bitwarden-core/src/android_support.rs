775b66ba337062e75132b2f923efdbbd8e41ba8b
Daniel García <dani-garcia@users.noreply.github.com>
2024-07-30 15:16:21 +0200

    Improve error handling and comments

--- crates/bitwarden-core/src/android_support.rs
+++ crates/bitwarden-core/src/android_support.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use jni::{
     signature::JavaType,
     sys::{jint, jsize, JavaVM},
@@ -5,10 +7,18 @@ use jni::{
 
 pub fn init() {
     static ANDROID_INIT: std::sync::Once = std::sync::Once::new();
+
+    fn init_inner() -> Result<(), Box<dyn Error>> {
+        let jvm = java_vm()?;
+        let env = jvm.attach_current_thread_permanently()?;
+        init_verifier(&env)?;
+        Ok(())
+    }
+
     ANDROID_INIT.call_once(|| {
-        let jvm = java_vm();
-        let env = jvm.attach_current_thread_permanently().unwrap();
-        init_verifier(&env).unwrap();
+        if let Err(e) = init_inner() {
+            log::error!("Failed to initialize Android support: {}", e);
+        }
     });
 }
 
@@ -16,50 +26,50 @@ type JniGetCreatedJavaVms =
     unsafe extern "system" fn(vmBuf: *mut *mut JavaVM, bufLen: jsize, nVMs: *mut jsize) -> jint;
 const JNI_GET_JAVA_VMS_NAME: &[u8] = b"JNI_GetCreatedJavaVMs";
 
-fn java_vm() -> jni::JavaVM {
-    // should be jni_onload，but uniffi use jna
+fn java_vm() -> Result<jni::JavaVM, Box<dyn Error>> {
+    // Ideally we would use JNI to get a reference to the JavaVM, but that's not possible since
+    // UniFFI uses JNA
+    //
+    // If we could use JNI, we'd just need to export a function and call it from the Android app:
+    // #[export_name = "Java_com_orgname_android_rust_init"]
+    // extern "C" fn java_init(env: JNIEnv, _class: JClass, context: JObject, ) -> jboolean {
+    //
+    // Instead we have to use libloading to get a reference to the JavaVM:
+    //
     // https://github.com/mozilla/uniffi-rs/issues/1778#issuecomment-1807979746
     let lib = libloading::os::unix::Library::this();
-    let get_created_java_vms: JniGetCreatedJavaVms =
-        unsafe { *lib.get(JNI_GET_JAVA_VMS_NAME).unwrap() };
+    let get_created_java_vms: JniGetCreatedJavaVms = unsafe { *lib.get(JNI_GET_JAVA_VMS_NAME)? };
 
-    let mut created_java_vms: [*mut JavaVM; 1] = [std::ptr::null_mut() as *mut JavaVM];
-    let mut java_vms_count: i32 = 0;
-    let ok = unsafe { get_created_java_vms(created_java_vms.as_mut_ptr(), 1, &mut java_vms_count) };
-    assert_eq!(ok, jni::sys::JNI_OK);
-    assert_eq!(java_vms_count, 1);
+    let mut java_vms: [*mut JavaVM; 1] = [std::ptr::null_mut() as *mut JavaVM];
+    let mut vm_count: i32 = 0;
 
-    let jvm_ptr = created_java_vms[0];
-    let jvm = unsafe { jni::JavaVM::from_raw(jvm_ptr) }.unwrap();
-    jvm
+    let ok = unsafe { get_created_java_vms(java_vms.as_mut_ptr(), 1, &mut vm_count) };
+    if ok != jni::sys::JNI_OK {
+        return Err("Failed to get JavaVM".into());
+    }
+    if vm_count != 1 {
+        return Err(format!("Invalid JavaVM count: {vm_count}").into());
+    }
+
+    let jvm = unsafe { jni::JavaVM::from_raw(java_vms[0]) }?;
+    Ok(jvm)
 }
 
 fn init_verifier(env: &jni::JNIEnv<'_>) -> jni::errors::Result<()> {
-    let activity_thread = "android/app/ActivityThread";
-    let current_activity_thread = env.get_static_method_id(
-        &activity_thread,
-        "currentActivityThread",
-        "()Landroid/app/ActivityThread;",
-    )?;
-    let at = env
-        .call_static_method_unchecked(
-            &activity_thread,
-            current_activity_thread,
-            JavaType::Object("android/app/ActivityThread".to_string()),
+    let activity_thread = env
+        .call_static_method(
+            "android/app/ActivityThread",
+            "currentActivityThread",
+            "()Landroid/app/ActivityThread;",
             &[],
         )?
         .l()?;
 
-    let get_application = env.get_method_id(
-        activity_thread,
-        "getApplication",
-        "()Landroid/app/Application;",
-    )?;
     let context = env
-        .call_method_unchecked(
-            at,
-            get_application,
-            JavaType::Object("android/app/Application".to_string()),
+        .call_method(
+            activity_thread,
+            "getApplication",
+            "()Landroid/app/Application;",
             &[],
         )?
         .l()?;

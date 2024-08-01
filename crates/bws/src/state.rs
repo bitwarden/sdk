8e3f6f43f7f430cd259bc05e7563e51c559f0419
Colton Hurst <colton@coltonhurst.com>
2024-08-01 16:59:08 -0400

    SM-1174: Fix default directory selection in bws

--- crates/bws/src/state.rs
+++ crates/bws/src/state.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
+use directories::BaseDirs;
 
 pub(crate) const DEFAULT_STATE_DIRECTORY: &str = ".config/bws/state";
 
@@ -10,7 +11,13 @@ pub(crate) fn get_state_file(
 ) -> Result<PathBuf> {
     let mut state_dir = match state_dir {
         Some(state_dir) => state_dir,
-        None => PathBuf::from(DEFAULT_STATE_DIRECTORY),
+        None => {
+            let Some(base_dirs) = BaseDirs::new() else {
+                return Err(eyre!("A valid home directory doesn't exist"));
+            };
+
+            base_dirs.home_dir().join(DEFAULT_STATE_DIRECTORY)
+        }
     };
 
     std::fs::create_dir_all(&state_dir)?;

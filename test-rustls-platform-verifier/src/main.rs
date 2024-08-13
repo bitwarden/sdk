22c341e2b456e8dc6d0b7e53d4f7871912afb3af
Thomas Avery <tavery@bitwarden.com>
2024-08-13 14:38:04 -0500

    edit test

--- test-rustls-platform-verifier/src/main.rs
+++ test-rustls-platform-verifier/src/main.rs
@@ -4,15 +4,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .use_preconfigured_tls(rustls_platform_verifier::tls_config())
         .build()
         .expect("Build should not fail");
+    let request = client.get("https://httpbin.org/ip").build()?;
+    let response = client.execute(request).await?;
 
-    let resp = client
-        .get("https://httpbin.org/ip")
-        .send()
-        .await?
-        .text()
-        .await?;
+    let status_code = response.status();
+    let content = response.text().await?;
 
-    println!("body = {resp:?}");
+    println!("status_code = {status_code:?}");
+    println!("content = {content:?}");
 
     Ok(())
 }

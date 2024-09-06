c44f52aedcd1a4f17c6e6c1d0a42e6a7e25db313
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-09-06 08:50:48 -0700

    tweak output format

--- languages/php/example.php
+++ languages/php/example.php
@@ -24,11 +24,11 @@ try {
 print("Projects:\n");
 $res = $bitwarden_client->projects->create($organization_id, 'php project');
 $project_id = $res->id;
-print("\tcreate: '" . $project_id . "'\n");
+print("\tcreate: '" . $project_id . "'\n\n");
 
 // get project
 $res = $bitwarden_client->projects->get($project_id);
-print("\tget: '" . $res->name . "'\n");
+print("\tget: '" . $res->name . "'\n\n");
 
 // list projects
 $res = $bitwarden_client->projects->list($organization_id);
@@ -36,6 +36,7 @@ print("\tlist:\n");
 foreach ($res->data as $project) {
     print("\t\tID: '" . $project->id . "', Name: '" . $project->name . "'\n");
 }
+print("\n");
 
 // update project
 $res = $bitwarden_client->projects->update($organization_id, $project_id, 'php test awesome');
@@ -43,38 +44,40 @@ print("\tupdate: '" . $res->name . "'\n\n");
 
 // sync secrets
 print("Secrets:\n");
-print("\tsyncing secrets...\n");
+print("\tSyncing secrets...\n");
 $res = $bitwarden_client->secrets->sync($organization_id,null);
 $now = new DateTime();
 $now_string = $now->format('Y-m-d\TH:i:s.u\Z');
-print("\t\tsync has changes: " . ($res->hasChanges ? 'true' : 'false') . "\n");
+print("\t\tSync has changes: " . ($res->hasChanges ? 'true' : 'false') . "\n\n");
 
-print("\tsyncing again to ensure no changes since last sync...\n");
+print("\tSyncing again to ensure no changes since last sync...\n");
 $res = $bitwarden_client->secrets->sync($organization_id, $now_string);
-print("\t\tsync has changes: " . ($res->hasChanges ? 'true' : 'false') . "\n");
+print("\t\tSync has changes: " . ($res->hasChanges ? 'true' : 'false') . "\n\n");
 
 // create secret
 $res = $bitwarden_client->secrets->create($organization_id, "New Key", "New value", "New note", [$project_id]);
 $secret_id = $res->id;
-print("\tcreate: '" . $secret_id . "'\n");
+print("\tcreate: '" . $secret_id . "'\n\n");
 
 // get secret
 $res = $bitwarden_client->secrets->get($secret_id);
-print("\tget: '" . $res->key . "'\n");
+print("\tget: '" . $res->key . "'\n\n");
 
 // get multiple secrets by ids
 $res = $bitwarden_client->secrets->get_by_ids([$secret_id]);
 print("\tget_by_ids:\n");
 foreach ($res->data as $secret) {
-    print("\t\tID: '" . $secret->id . "', Name: '" . $secret->key . "'\n");
+    print("\t\tID: '" . $secret->id . "', Key: '" . $secret->key . "'\n");
 }
+print("\n");
 
 // list secrets
 $res = $bitwarden_client->secrets->list($organization_id);
 print("\tlist:\n");
 foreach ($res->data as $secret) {
-    print("\t\tID: '" . $secret->id . "', Name: '" . $secret->key . "'\n");
+    print("\t\tID: '" . $secret->id . "', Key: '" . $secret->key . "'\n");
 }
+print("\n");
 
 // update secret
 $res = $bitwarden_client->secrets->update($organization_id, $secret_id, "Updated key", "Updated value", "Updated note", [$project_id]);
@@ -85,12 +88,14 @@ print("Cleaning up secrets and projects:\n");
 $res = $bitwarden_client->secrets->delete([$secret_id]);
 print("\tdelete:\n");
 foreach ($res->data as $secret) {
-    print("\t\tdeleted secret: '" . $secret->id . "'\n\n");
+    print("\t\tdeleted secret: '" . $secret->id . "'\n");
 }
+print("\n");
 
 // delete project
 $res = $bitwarden_client->projects->delete([$project_id]);
 print("\tdelete:\n");
 foreach ($res->data as $project) {
-    print("\t\tdeleted project: '" . $project->id . "'\n\n");
+    print("\t\tdeleted project: '" . $project->id . "'\n");
 }
+print("\n");

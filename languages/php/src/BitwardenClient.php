ef6a72583f746f0b8662edffdd0d5af6ef66bd3d
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-09-06 10:45:58 -0700

    move loginaccesstoken into authclient

--- languages/php/src/BitwardenClient.php
+++ languages/php/src/BitwardenClient.php
@@ -6,36 +6,12 @@ use Bitwarden\Sdk\Schemas\AccessTokenLoginRequest;
 use Bitwarden\Sdk\schemas\ClientSettings;
 use Bitwarden\Sdk\Schemas\Command;
 use FFI;
-use Swaggest\JsonDiff\Exception;
 
-
-class BitwardenClient
-{
-    private BitwardenLib $bitwarden_lib;
-
-    private ClientSettings $clientSettings;
-
-    public ProjectsClient $projects;
-
-    public SecretsClient $secrets;
-
+class AuthClient {
     private CommandRunner $commandRunner;
 
-    private FFI\CData $handle;
-
-    public function __construct(BitwardenSettings $bitwardenSettings)
-    {
-        $this->clientSettings = new ClientSettings();
-        $this->clientSettings->apiUrl = $bitwardenSettings->get_api_url();
-        $this->clientSettings->identityUrl = $bitwardenSettings->get_identity_url();
-        $this->clientSettings->userAgent = "Bitwarden PHP-SDK";
-
-        $this->bitwarden_lib = new BitwardenLib();
-        $this->handle = $this->bitwarden_lib->init($this->clientSettings);
-
-        $this->commandRunner = new CommandRunner($this->bitwarden_lib, $this->handle);
-        $this->projects = new ProjectsClient($this->commandRunner);
-        $this->secrets = new SecretsClient($this->commandRunner);
+    public function __construct(CommandRunner $commandRunner) {
+        $this->commandRunner = $commandRunner;
     }
 
     /**
@@ -56,6 +32,44 @@ class BitwardenClient
         if ($result->authenticated == False) {
             throw new \Exception("Unauthorized");
         }
+    }
+}
+
+class BitwardenClient
+{
+    private BitwardenLib $bitwarden_lib;
+
+    private ClientSettings $clientSettings;
+
+    public ProjectsClient $projects;
+
+    public SecretsClient $secrets;
+
+    private CommandRunner $commandRunner;
+
+    private ?AuthClient $authClient;
+
+    private FFI\CData $handle;
+
+    public function __construct(BitwardenSettings $bitwardenSettings)
+    {
+        $this->clientSettings = new ClientSettings();
+        $this->clientSettings->apiUrl = $bitwardenSettings->get_api_url();
+        $this->clientSettings->identityUrl = $bitwardenSettings->get_identity_url();
+        $this->clientSettings->userAgent = "Bitwarden PHP-SDK";
+
+        $this->bitwarden_lib = new BitwardenLib();
+        $this->handle = $this->bitwarden_lib->init($this->clientSettings);
+
+        $this->commandRunner = new CommandRunner($this->bitwarden_lib, $this->handle);
+        $this->projects = new ProjectsClient($this->commandRunner);
+        $this->secrets = new SecretsClient($this->commandRunner);
+    }
+
+    public function auth(): AuthClient
+    {
+        $this->authClient = new AuthClient($this->commandRunner);
+        return $this->authClient;
     }
 
     public function __destruct()

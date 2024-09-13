84bd3205d080e321c1f1ccabe826175d0823d8a5
Colton Hurst <colton@coltonhurst.com>
2024-09-13 16:39:08 -0400

    SM-1425: clean up index.js

--- languages/js/example/index.js
+++ languages/js/example/index.js
@@ -14,42 +14,46 @@ async function main() {
   await client.auth().loginAccessToken(process.env.ACCESS_TOKEN);
   const organization_id = process.env.ORGANIZATION_ID;
 
+  // Project functions
+
   const project = await client.projects().create(organization_id, "project-name");
-  const projects = await client.projects().list(organization_id);
-  console.log(projects.data[0]);
+  console.log(project);
 
   const project_get = await client.projects().get(project.id);
   console.log(project_get);
 
-  const updated_project = await client.projects().update(organization_id, project.id, "updated-project-name");
+  const projects = await client.projects().list(organization_id);
+  console.log(projects.data);
+
+  const updated_project = await client.projects().update(organization_id, project.id, "project-name-updated");
   console.log(updated_project);
+
+  // Secret functions
 
   const secret = await client
     .secrets()
-    .create(organization_id, "test-secret", "test-value", "test-note", [project.id]);
-  const secrets = await client.secrets().list(organization_id);
-  console.log(secrets.data);
+    .create(organization_id, "secret-key", "secret-value", "secret-note", [project.id]);
+  console.log(secret);
 
-  const secretData = await client.secrets().get(secret.id);
+  const secret_get = await client.secrets().get(secret.id);
+  console.log(secret_get);
+
+  const secrets = await client.secrets().list(organization_id);
   console.log(secrets.data);
 
   const secretsByIds = await client.secrets().getByIds([secret.id]);
   console.log(secretsByIds.data);
 
-  const secret_sync_has_changes = await client.secrets().sync(organization_id, null);
-  const now = new Date();
-  console.log(secret_sync_has_changes.hasChanges);
-
-  const updated_secret = await client.secrets().update(organization_id, secret.id, "updated-secret", "updated-value", "updated-note", [project.id]);
+  const updated_secret = await client.secrets().update(organization_id, secret.id, "secret-key-updated", "secret-value-updated", "secret-note-updated", [project.id]);
   console.log(updated_secret);
 
-  const sync_has_changes_after_update = await client.secrets().sync(organization_id, now);
-  console.log(sync_has_changes_after_update.hasChanges);
+  const now = new Date();
+  const secret_sync = await client.secrets().sync(organization_id, now);
+  console.log(secret_sync.hasChanges);
 
-  for (const secret of secrets.data) {
-    await client.secrets().delete([secret.id]);
-  }
+  // Delete functions
 
+  await client.secrets().delete([secret.id]);
   await client.projects().delete([project.id]);
 }
 

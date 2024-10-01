eac218c72d6d5f3c3e6b2afadf370760fc1cda5d
Bernd Schoolmann <mail@quexten.com>
2024-10-01 16:37:48 +0200

    Add documentation for ssh key formats

--- crates/bitwarden-vault/src/cipher/ssh_key.rs
+++ crates/bitwarden-vault/src/cipher/ssh_key.rs
@@ -8,8 +8,11 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
 pub struct SshKey {
+    /// Ssh private key (ed25519/rsa) in unencrypted openssh private key format https://github.com/openssh/openssh-portable/blob/master/PROTOCOL.key
     pub private_key: Option<EncString>,
+    /// Ssh public key (ed25519/rsa) according to RFC4253 https://datatracker.ietf.org/doc/html/rfc4253#section-6.6
     pub public_key: Option<EncString>,
+    /// Ssh fingerprint using SHA256 in the format: SHA256:BASE64_ENCODED_FINGERPRINT
     pub fingerprint: Option<EncString>,
 }
 
@@ -17,8 +20,11 @@ pub struct SshKey {
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
 pub struct SshKeyView {
+    /// Ssh private key (ed25519/rsa) in unencrypted openssh private key format https://github.com/openssh/openssh-portable/blob/master/PROTOCOL.key
     pub private_key: Option<String>,
+    /// Ssh public key (ed25519/rsa) according to RFC4253 https://datatracker.ietf.org/doc/html/rfc4253#section-6.6
     pub public_key: Option<String>,
+    /// Ssh fingerprint using SHA256 in the format: SHA256:BASE64_ENCODED_FINGERPRINT
     pub fingerprint: Option<String>,
 }
 

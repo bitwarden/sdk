d4dac0da5be4e54d5c4acde6ddc95ab8d89189e5
Hinton <hinton@users.noreply.github.com>
2024-10-04 10:52:18 +0200

    Fix publish internal

--- crates/bitwarden-wasm-internal/build.sh
+++ crates/bitwarden-wasm-internal/build.sh
@@ -6,12 +6,10 @@ if [ "$1" != "-r" ]; then
   # Dev
   cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown
   wasm-bindgen --target bundler --out-dir languages/js/sdk-internal ./target/wasm32-unknown-unknown/debug/bitwarden_wasm_internal.wasm
-  wasm-bindgen --target nodejs --out-dir languages/js/sdk-internal/node ./target/wasm32-unknown-unknown/debug/bitwarden_wasm_internal.wasm
 else
   # Release
   cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown --release
   wasm-bindgen --target bundler --out-dir languages/js/sdk-internal ./target/wasm32-unknown-unknown/release/bitwarden_wasm_internal.wasm
-  wasm-bindgen --target nodejs --out-dir languages/js/sdk-internal/node ./target/wasm32-unknown-unknown/release/bitwarden_wasm_internal.wasm
 fi
 
 # Format
@@ -19,7 +17,6 @@ npx prettier --write ./languages/js/sdk-internal
 
 # Optimize size
 wasm-opt -Os ./languages/js/sdk-internal/bitwarden_wasm_internal_bg.wasm -o ./languages/js/sdk-internal/bitwarden_wasm_internal_bg.wasm
-wasm-opt -Os ./languages/js/sdk-internal/node/bitwarden_wasm_internal_bg.wasm -o ./languages/js/sdk-internal/node/bitwarden_wasm_internal_bg.wasm
 
 # Transpile to JS
 wasm2js ./languages/js/sdk-internal/bitwarden_wasm_internal_bg.wasm -o ./languages/js/sdk-internal/bitwarden_wasm_internal_bg.wasm.js

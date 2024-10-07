8860dfdfe876b6f0539e2b56c0e65b3ac07699f3
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-10-07 12:11:57 +0200

    feat: add error handling to `ClientCrypto` calls

--- crates/bitwarden-wasm-internal/src/crypto.rs
+++ crates/bitwarden-wasm-internal/src/crypto.rs
@@ -6,7 +6,7 @@ use bitwarden::{
 };
 use wasm_bindgen::prelude::*;
 
-// use crate::{error::Result, Client};
+use crate::error::Result;
 
 #[wasm_bindgen]
 pub struct ClientCrypto(pub(crate) Rc<Client>);
@@ -15,19 +15,23 @@ pub struct ClientCrypto(pub(crate) Rc<Client>);
 impl ClientCrypto {
     /// Initialization method for the user crypto. Needs to be called before any other crypto
     /// operations.
-    pub async fn initialize_user_crypto(&self, req: InitUserCryptoRequest) {
-        // Ok(
-        self.0.crypto().initialize_user_crypto(req).await.unwrap()
-        // );
-        // .map_err(Error::EncryptionSettings)?)
+    pub async fn initialize_user_crypto(&self, req: InitUserCryptoRequest) -> Result<()> {
+        Ok(self
+            .0
+            .crypto()
+            .initialize_user_crypto(req)
+            .await
+            .map_err(bitwarden_core::Error::EncryptionSettings)?)
     }
 
     /// Initialization method for the organization crypto. Needs to be called after
     /// `initialize_user_crypto` but before any other crypto operations.
-    pub async fn initialize_org_crypto(&self, req: InitOrgCryptoRequest) {
-        // Ok(
-        self.0.crypto().initialize_org_crypto(req).await.unwrap()
-        // );
-        // .map_err(Error::EncryptionSettings)?)
+    pub async fn initialize_org_crypto(&self, req: InitOrgCryptoRequest) -> Result<()> {
+        Ok(self
+            .0
+            .crypto()
+            .initialize_org_crypto(req)
+            .await
+            .map_err(bitwarden_core::Error::EncryptionSettings)?)
     }
 }

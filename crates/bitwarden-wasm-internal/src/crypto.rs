2db787de45525986bc64485b7de20b49dab66865
Thomas Avery <tavery@bitwarden.com>
2024-10-14 11:38:20 -0500

    Add methods to wasm-internal

--- crates/bitwarden-wasm-internal/src/crypto.rs
+++ crates/bitwarden-wasm-internal/src/crypto.rs
@@ -1,7 +1,10 @@
 use std::rc::Rc;
 
 use bitwarden_core::{
-    mobile::crypto::{InitOrgCryptoRequest, InitUserCryptoRequest},
+    mobile::crypto::{
+        InitOrgCryptoRequest, InitUserCryptoRequest, MakeKeyPairResponse,
+        VerifyAsymmetricKeysRequest, VerifyAsymmetricKeysResponse,
+    },
     Client,
 };
 use wasm_bindgen::prelude::*;
@@ -29,5 +32,16 @@ impl ClientCrypto {
     /// `initialize_user_crypto` but before any other crypto operations.
     pub async fn initialize_org_crypto(&self, req: InitOrgCryptoRequest) -> Result<()> {
         Ok(self.0.crypto().initialize_org_crypto(req).await?)
+    }
+
+    pub fn make_key_pair(&self) -> Result<MakeKeyPairResponse> {
+        Ok(self.0.crypto().make_key_pair()?)
+    }
+
+    pub fn verify_asymmetric_keys(
+        &self,
+        request: VerifyAsymmetricKeysRequest,
+    ) -> Result<VerifyAsymmetricKeysResponse> {
+        Ok(self.0.crypto().verify_asymmetric_keys(request)?)
     }
 }
